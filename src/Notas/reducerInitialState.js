2d160fc9690f769d7a931cd7b08534b401554384
Marianela <marianelaclara@gmail.com>
2022-03-03 16:51:03 -0300

    Pongo en 0 el estado inicial: no hay notas

--- src/Notas/reducerInitialState.js
+++ src/Notas/reducerInitialState.js
@@ -1,16 +1,5 @@
 const initialState = {
-    notas: [
-        { title: 'Titulo 1', body: 'Cuerpo de la nota 1', id: 1 },
-        { title: 'Titulo 2', body: 'Cuerpo de la nota 2', id: 2 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 3 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 4 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 5 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 6 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 7 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 8 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 9 },
-        { title: 'Titulo 3', body: 'Cuerpo de la nota 3', id: 10 },
-      ],
+    notas: [],
     apiLoading: false,
 };
 
