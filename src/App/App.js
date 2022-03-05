0fa017cdaf56eeffd57ca205154bc7595e5cceea
Marianela <marianelaclara@gmail.com>
2022-03-05 15:58:15 -0300

    Creo la componente para el  login

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import NotasContainer from '../Notas/components/NotasContainer';
 import Ayuda from '../Ayuda/components/Ayuda';
 import NotaCreate from '../Notas/components/NotaCreate';
 import NavBar from '../Notas/components/NavBar';
+import LoginPage from '../Login/LoginPage';
 import { FETCH_NOTAS } from '../Notas/actionTypes';
 
 function App() {
@@ -18,7 +19,9 @@ function App() {
                 <Route path='/mis-notas' element={ <NotasContainer notas={ notas } /> } />
                 <Route path='/help' element={ <Ayuda /> } />
                 <Route path='/create-notas' element={ <NotaCreate /> } />
+                <Route path='/login' element={ <LoginPage /> } />
             </Routes>
+            
         </div>
     );
 }
